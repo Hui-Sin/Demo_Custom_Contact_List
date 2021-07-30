8d60c78cf33255a58cd180efd96107602304a2a5
Hui-Sin <20018354@myrp.edu.sg>
2021-07-30 10:00:46 +0800

    Section B

--- app/src/main/java/sg/edu/rp/c346/id20018354/democustomcontactlist/CustomAdapter.java
+++ app/src/main/java/sg/edu/rp/c346/id20018354/democustomcontactlist/CustomAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -33,6 +34,7 @@ public class CustomAdapter extends ArrayAdapter {
         TextView tvName = rowView.findViewById(R.id.textViewName);
         TextView tvCode = rowView.findViewById(R.id.textViewCountryCode);
         TextView tvNum = rowView.findViewById(R.id.textViewPhoneNum);
+        ImageView ivGender = rowView.findViewById(R.id.imageViewGender);
 
 
         // Obtain the Android Version information based on the position
@@ -42,6 +44,11 @@ public class CustomAdapter extends ArrayAdapter {
         tvName.setText(currentItem.getName());
         tvCode.setText("+" + currentItem.getCountryCode());
         tvNum.setText(currentItem.getPhoneNum() + "");
+        if (currentItem.getGender()=='F') {
+            ivGender.setImageResource(R.drawable.female);
+        }else {
+            ivGender.setImageResource(R.drawable.male);
+        }
 
         return rowView;
     }
